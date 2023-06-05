f040d5b1a402c1063b19988777cee701cf9ff6de
khanhzom <khanhzom@gmail.com>
2023-06-05 08:45:32 -0700

    project1

---
+++ project python web ban hang/readme.txt
@@ -0,0 +1,4 @@
+1 Open in the compiler any code that supports Python and DINGO.
+2. Open Terminal on the code compiler
+3. Cd to the path \python\webbanhang>
+4. Run the command python manage.py runserver
---
+++ project python web ban hang/webbanhang/app/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home,name="home"),
+    path('register/', views.register, name="register"),
+    path('login/', views.loginPage, name="login"),
+    path('category/', views.category, name="category"),
+    path('detail/', views.detail, name="detail"),
+    path('logout/', views.logoutPage, name="logout"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
+    path('update_item/', views.updateItem, name="update_item"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+
+
+
+    
+]
